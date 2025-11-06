a795c70b480850a97475bbbff6b0a05e0d6f5c43
Vinay Phougat <vinayphougat20@gmail.com>
2025-11-06 00:38:43 -0500

    Replaced old project with new files

--- src/components/PostCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function PostCard({title, body}) {
-    return <div className="postCard">
-        {/* Basic Post Outline, might modify in the future*/}
-        <h3>{title}</h3>
-        <p>{body}</p>
-    </div>
-}
